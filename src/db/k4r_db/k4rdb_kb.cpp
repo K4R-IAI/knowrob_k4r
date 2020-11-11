17fdd1e1f49de069717fa4c21ee573318f215a5d
Sascha Jongebloed <sasjonge@uni-bremen.de>
2020-11-11 18:45:13 +0100

    Added example code

--- src/db/k4r_db/k4rdb_kb.cpp
+++ src/db/k4r_db/k4rdb_kb.cpp
@@ -12,8 +12,12 @@
 #include <list>    
 
 PREDICATE(k4rdb_values, 2) {
-	// This method gets the path in the variable PL_A1 and should return a list of values and assign it toPL_A2
-	// Returning could look a bit like this:
+	// This method gets the path in the variable PL_A1.
+	// This is how you assign the value to path
+	std::string path((char*)PL_A1);
+	// Then get the values from the API
+	// Then it should return a list of the values and assign it to PL_A2
+	// Returning lists to Prolog can be done like this:
 	PlTail values(PL_A2);
 	values.append("Test1");
 	values.append("Test2");
