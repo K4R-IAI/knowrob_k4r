2b9110676833b6958e99ded2d537f4abd5a65fd7
Nils Leusmann <leusmann@uni-bremen.de>
2020-11-12 13:46:02 +0100

    Adds some additional Wrapper Predicates

--- src/db/sap/sap_plugin.pl
+++ src/db/sap/sap_plugin.pl
@@ -3,7 +3,13 @@
 	sap_mng_wipe/0,
 	sap_mng_get_item_filter/3,
 	sap_mng_get_field_values/2,
-	sap_mng_get_price_of_MATNR/2
+	sap_mng_get_price_of_matnr/2,
+	sap_mng_get_shelf_of_matnr/2,
+	sap_mng_get_width_of_matnr/2,
+	sap_mng_get_height_of_matnr/2,
+	sap_mng_get_lenght_of_matnr/2,
+	sap_mng_get_sortf_of_matnr/2,
+	sap_mng_get_ammountfacings_of_matnr/2
 	]).
 
 :- use_module(library('db/mongo/client')).
@@ -74,14 +80,77 @@ sap_mng_get_item_filter(Field,FieldValue,Items) :-
 	), Items),
 	mng_cursor_destroy(Cursor).
 
-sap_mng_get_price_of_MATNR(MATNR,Price) :-
+sap_mng_get_price_of_matnr(MATNR,Price) :-
 	mng_db_name(DB),
 	sap_db(DB, Name),
 	mng_cursor_create(DB,Name,Cursor),
-	mng_cursor_filter(Cursor,['MATNR',MATNR]),
+	mng_cursor_filter(Cursor,['MATNR',string(MATNR)]),
 	mng_cursor_materialize(Cursor,Item),
 	mng_get_dict('E1WBB03',Item,ProductInfos),
 	mng_get_dict('E1WBB07',ProductInfos,ProductInfosPricerelated),
 	mng_get_dict('E1WBB08',ProductInfosPricerelated,ProductInfosPriceInfo),
-	mng_get_dict('KWERT',ProductInfosPriceInfo,Price),
+	mng_get_dict('KWERT',ProductInfosPriceInfo,string(Price)),
+	mng_cursor_destroy(Cursor).
+
+%----
+sap_mng_get_shelf_of_matnr(MATNR,Shelf) :-
+	mng_db_name(DB),
+	sap_db(DB, Name),
+	mng_cursor_create(DB,Name,Cursor),
+	mng_cursor_filter(Cursor,['MATNR',string(MATNR)]),
+	mng_cursor_materialize(Cursor,Item),
+	mng_get_dict('E1WBB18',Item,LayoutInfos),
+	mng_get_dict('SHELF',LayoutInfos,string(Shelf)),
+	mng_cursor_destroy(Cursor).
+
+
+sap_mng_get_width_of_matnr(MATNR,Width) :-
+	mng_db_name(DB),
+	sap_db(DB, Name),
+	mng_cursor_create(DB,Name,Cursor),
+	mng_cursor_filter(Cursor,['MATNR',string(MATNR)]),
+	mng_cursor_materialize(Cursor,Item),
+	mng_get_dict('E1WBB03',Item,ProductInfos),
+	mng_get_dict('BREIT',ProductInfos,string(Width)),
+	mng_cursor_destroy(Cursor).
+
+
+sap_mng_get_height_of_matnr(MATNR,Height) :-
+	mng_db_name(DB),
+	sap_db(DB, Name),
+	mng_cursor_create(DB,Name,Cursor),
+	mng_cursor_filter(Cursor,['MATNR',string(MATNR)]),
+	mng_cursor_materialize(Cursor,Item),
+	mng_get_dict('E1WBB03',Item,ProductInfos),
+	mng_get_dict('HOEHE',ProductInfos,string(Height)),
+	mng_cursor_destroy(Cursor).
+
+sap_mng_get_lenght_of_matnr(MATNR,Lenght) :-
+	mng_db_name(DB),
+	sap_db(DB, Name),
+	mng_cursor_create(DB,Name,Cursor),
+	mng_cursor_filter(Cursor,['MATNR',string(MATNR)]),
+	mng_cursor_materialize(Cursor,Item),
+	mng_get_dict('E1WBB03',Item,ProductInfos),
+	mng_get_dict('LAENG',ProductInfos,string(Lenght)),
+	mng_cursor_destroy(Cursor).
+
+sap_mng_get_sortf_of_matnr(MATNR,Sortfolge) :-
+	mng_db_name(DB),
+	sap_db(DB, Name),
+	mng_cursor_create(DB,Name,Cursor),
+	mng_cursor_filter(Cursor,['MATNR',string(MATNR)]),
+	mng_cursor_materialize(Cursor,Item),
+	mng_get_dict('E1WBB18',Item,LayoutInfos),
+	mng_get_dict('SORF1',LayoutInfos,string(Sortfolge)),
+	mng_cursor_destroy(Cursor).
+
+sap_mng_get_ammountfacings_of_matnr(MATNR,AnzFacings) :-
+	mng_db_name(DB),
+	sap_db(DB, Name),
+	mng_cursor_create(DB,Name,Cursor),
+	mng_cursor_filter(Cursor,['MATNR',string(MATNR)]),
+	mng_cursor_materialize(Cursor,Item),
+	mng_get_dict('E1WBB18',Item,LayoutInfos),
+	mng_get_dict('FACIN',LayoutInfos,string(AnzFacings)),
 	mng_cursor_destroy(Cursor).
--- src/db/sap/sap_plugin.plt
+++ src/db/sap/sap_plugin.plt
@@ -28,24 +28,38 @@ test('sap_mng_get_item_filter') :-
 test('sap_mng_get_price_of_MATNR') :-
   write(-------------------------------),
   write('\n'),
-  sap_mng_get_price_of_MATNR(string('000000000100000022'),Price),
+  sap_mng_get_price_of_matnr('000000000100000022',Price),
   write(Price),
   write('\n').
-  %DB=roslog,
-  %Name=sap,
-  %P1=,
-  %write(-------------------------------),
-  %mng_db_name(DB),
-  %mng_cursor_create(DB,Name,Cursor),
-  %mng_cursor_filter(Cursor,['MATNR',P1]),
-  %mng_cursor_materialize(Cursor,Item),
-  %write('Item:\n'),
-  %write(Item),
-  %write('\n'),
-  %mng_get_dict('E1WBB03',Item,ProductInfos),
-  %mng_get_dict('E1WBB07',ProductInfos,ProductInfosPricerelated),
-  %mng_get_dict('E1WBB08',ProductInfosPricerelated,ProductInfosPriceInfo),
-  %mng_get_dict('KWERT',ProductInfosPriceInfo,Price),
-  %write(Price).
+
+test(sap_mng_get_shelf_of_matnr) :-
+  sap_mng_get_shelf_of_matnr('000000000100000022',Shelf),
+  write(Shelf),
+  write('\n').
+
+test(sap_mng_get_width_of_matnr):-
+  sap_mng_get_width_of_matnr('000000000100000022',Width),
+  write(Width),
+  write('\n').
+
+test(sap_mng_get_height_of_matnr) :-
+  sap_mng_get_height_of_matnr('000000000100000022',Height),
+  write(Height),
+  write('\n').
+
+test(sap_mng_get_lenght_of_matnr):-
+  sap_mng_get_lenght_of_matnr('000000000100000022',Lenght),
+  write(Lenght),
+  write('\n').
+
+test(sap_mng_get_sortf_of_matnr):-
+  sap_mng_get_sortf_of_matnr('000000000100000022',Sortf),
+  write(Sortf),
+  write('\n').
+
+test(sap_mng_get_ammountfacings_of_matnr):-
+  sap_mng_get_ammountfacings_of_matnr('000000000100000022',Facings),
+  write(Facings),
+  write('\n').
 
 :- end_tests('sap_plugin')
