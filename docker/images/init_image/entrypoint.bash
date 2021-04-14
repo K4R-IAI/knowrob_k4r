39e15c33c5fce8a348acfe54be700c2ee9ce0730
Giang Nguyen <hoanggia@uni-bremen.de>
2021-04-14 11:33:06 +0200

    Add knowrob-init

--- docker/images/init_image/entrypoint.bash
+++ docker/images/init_image/entrypoint.bash
@@ -1,5 +1,4 @@
 #!/bin/bash
-source /home/ros/devel/setup.bash
 mongorestore --uri=mongodb://mongo:27017/roslog /home/data/triples/
 mongorestore --uri=mongodb://mongo:27017/roslog /home/data/ros_tf/
 mongorestore --uri=mongodb://mongo:27017/roslog /home/data/annotations/
