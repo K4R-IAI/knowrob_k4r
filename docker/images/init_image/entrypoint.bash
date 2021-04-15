66be3f517cd5973cdb5b9e87c076b892a8ca5766
Giang Nguyen <hoanggia@uni-bremen.de>
2021-04-15 14:36:53 +0200

    Parameterize everything

--- docker/images/init_image/entrypoint.bash
+++ docker/images/init_image/entrypoint.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
-mongorestore --uri=mongodb://mongo:27017/roslog /home/data/triples/
-mongorestore --uri=mongodb://mongo:27017/roslog /home/data/ros_tf/
-mongorestore --uri=mongodb://mongo:27017/roslog /home/data/annotations/
-mongorestore --uri=mongodb://mongo:27017/roslog /home/data/inferred/
+mongorestore --uri=$KNOWROB_MONGODB_PKG_URI /home/data/triples/
+mongorestore --uri=$KNOWROB_MONGODB_PKG_URI /home/data/ros_tf/
+mongorestore --uri=$KNOWROB_MONGODB_PKG_URI /home/data/annotations/
+mongorestore --uri=$KNOWROB_MONGODB_PKG_URI /home/data/inferred/
 
 exec "$@"
