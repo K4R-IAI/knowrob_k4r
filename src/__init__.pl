f87c0e149430a7589d6e3e10d58a5f3d55df8291
¨kaviyachandran¨ <kaviya.dhanabalachandran@gmail.com>
2021-11-03 16:47:04 +0100

    Moved k4r_db to src folder

--- src/__init__.pl
+++ src/__init__.pl
@@ -3,7 +3,7 @@
 :- register_ros_package(knowrob_refills).
 :- register_ros_package(knowrob_k4r).
 
-:- use_directory('db').
+:- use_directory('k4r_db').
 
 :- use_module('./customer.pl').
 :- use_module('./products.pl').
--- src/k4r_db/Utilities/environment.cpp
+++ src/k4r_db/Utilities/environment.cpp
@@ -3,13 +3,13 @@
 #ifdef SANDBOX
     #define URL "https://dt-api.sandbox.knowledge4retail.org/k4r/api/v0/"
     #define GRAPHQL_URL "https://dt-api.sandbox.knowledge4retail.org/k4r/graphql"
-    #define CERT_PATH "/src/db/k4r_db/K4R_DEV_CERTs/k4r-sandbox-client.p12"
+    #define CERT_PATH "/src/k4r_db/K4R_DEV_CERTs/k4r-sandbox-client.p12"
     #define CERT_PASSWD "q7WgPL3OnopoyU4abkrw97LD3iqD"
     #define VERIFY_PEER false
 #else
     #define URL "https://dt-api.dev.knowledge4retail.org/k4r/api/v0/"
     #define GRAPHQL_URL "https://dt-api.dev.knowledge4retail.org/k4r/graphql"
-    #define CERT_PATH "/src/db/k4r_db/K4R_DEV_CERTs/k4r-dev-keystore.p12"
+    #define CERT_PATH "/src/k4r_db/K4R_DEV_CERTs/k4r-dev-keystore.p12"
     #define CERT_PASSWD "8FdseHr0wCHwfuDmMv7QdKYWvnZg"
     #define VERIFY_PEER true
 #endif
