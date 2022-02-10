ae649699ca2417934f0d8eab2a76a95266764b84
¨kaviyachandran¨ <kaviya.dhanabalachandran@gmail.com>
2022-02-10 13:31:31 +0100

    Use the right environment variables

--- src/k4r_db/Entities/DataController.cpp
+++ src/k4r_db/Entities/DataController.cpp
@@ -21,20 +21,20 @@ public:
     if (sandbox)
     {
       this->link = SANDBOX_URL + std::string(link_tail);
-      this->request.setOpt<curlpp::options::SslVerifyPeer>(VERIFY_PEER);
+      this->request.setOpt<curlpp::options::SslVerifyPeer>(SANDBOX_VERIFY_PEER);
       this->request.setOpt(new curlpp::options::SslCertType(CERT_TYPE));
-      std::string knowrob_k4r_path = ros::package::getPath("knowrob_k4r") + CERT_PATH;
+      std::string knowrob_k4r_path = ros::package::getPath("knowrob_k4r") + SANDBOX_CERT_PATH;
       this->request.setOpt(new curlpp::options::SslCert(knowrob_k4r_path));
-      this->request.setOpt(new curlpp::options::SslCertPasswd(CERT_PASSWD));
+      this->request.setOpt(new curlpp::options::SslCertPasswd(SANDBOX_CERT_PASSWD));
     }
     else
     {
       this->link = DEV_URL + std::string(link_tail);
-      this->request.setOpt<curlpp::options::SslVerifyPeer>(VERIFY_PEER);
+      this->request.setOpt<curlpp::options::SslVerifyPeer>(DEV_VERIFY_PEER);
       this->request.setOpt(new curlpp::options::SslCertType(CERT_TYPE));
-      std::string knowrob_k4r_path = ros::package::getPath("knowrob_k4r") + CERT_PATH;
+      std::string knowrob_k4r_path = ros::package::getPath("knowrob_k4r") + DEV_CERT_PATH;
       this->request.setOpt(new curlpp::options::SslCert(knowrob_k4r_path));
-      this->request.setOpt(new curlpp::options::SslCertPasswd(CERT_PASSWD));
+      this->request.setOpt(new curlpp::options::SslCertPasswd(DEV_CERT_PASSWD));
     }
   }
 
