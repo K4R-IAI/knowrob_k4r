f01db8087d89896eaa56aa35b007ea316b54f9b9
¨kaviyachandran¨ <kaviya.dhanabalachandran@gmail.com>
2022-02-21 18:03:46 +0100

    util predicates for conversion from other units to m

--- src/pa4/utils.pl
+++ src/pa4/utils.pl
@@ -5,7 +5,11 @@
 */
 
 :- module( utils,
-    [ list_to_string/2
+    [ list_to_string/2,
+    string_to_list/2,
+    convert_to_m/3,
+    is_list_empty_/1,
+    is_string_empty_/1
     ]
     ).
 
@@ -20,3 +24,16 @@ string_to_list(StringVal, List) :-
         (member(A, AtomList),
         atom_number(A, Anum)),
     List).
+
+is_list_empty_([]).
+
+is_string_empty_("").
+
+convert_to_m(1, In, Out) :- % 1 is m
+    Out is In.
+
+convert_to_m(2, In, Out) :- % 2 is mm
+    Out is In/1000.
+
+convert_to_m(3, In, Out) :- % 3 is cm
+    Out is In/100.
