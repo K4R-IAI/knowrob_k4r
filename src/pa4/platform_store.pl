3f2b17284ab91223681aeae982b46e47a5c1ecb1
¨kaviyachandran¨ <kaviya.dhanabalachandran@gmail.com>
2022-06-28 15:22:18 +0200

    fix the mesh paths

--- src/pa4/platform_store.pl
+++ src/pa4/platform_store.pl
@@ -11,9 +11,10 @@ create_store(StoreNum) :-
         (object_dimensions(Frame, D, W, H),
         H1 is H*100, D1 is D*10, W1 is W*10, 
         W2 is truncate(W1), D2 is truncate(D1), H2 is truncate(H1),
-        get_valid_shelf(H2, W2, D2, W3, D3),
+        get_valid_shelf(H2, W2, D2, W3, D3, Prefix),
         atomics_to_string(['ShelfSystemH', H2, 'T', W3, 'L', D3, 'W'], Type), 
-        atomics_to_string(['package://refills_models/shelf_models/', Type, '/', Type, '.dae'], Path),
+        (ground(Prefix) -> atomics_to_string(['package://refills_models/shelf_models/', Type, '/', Prefix, Type, '.dae'], Path);
+        atomics_to_string(['package://refills_models/shelf_models/', Type, '/', Type, '.dae'], Path)),
         assert_mesh_path(Frame, Path),
         assert_path_for_all_layers(Frame, D3, W3)
     )
@@ -32,52 +33,53 @@ assert_path_for_all_layers(Frame, Depth, Width) :-
             triple(Layer, shop:erpShelfLayerId, 1) -> tell(has_type(Layer, dmshop:'DMShelfBFloor')),
             atomics_to_string(['ShelfLayer', D, 'TilesL', W, '_Bottom'], Type)
             );
-            (get_valid_layer(Depth, Width, D, W),
+            (get_valid_layer(Depth, Width, D, W, Prefix),
                 atomics_to_string(['ShelfLayer', D, 'TilesL', W], Type)
             )),
-            atomics_to_string(['package://refills_models/shelf_models/', Type, '/', Type, '.dae'], Path),
+            (ground(Prefix) -> atomics_to_string(['package://refills_models/shelf_models/', Type, '/', Prefix, Type, '.dae'], Path);
+        atomics_to_string(['package://refills_models/shelf_models/', Type, '/', Type, '.dae'], Path)
+            ),
             assert_mesh_path(Layer, Path)
         )).
 
-get_valid_shelf(180, 5, 6, 5, 6).
-get_valid_shelf(180, 5, 10, 5, 10).
-get_valid_shelf(180, _, _, W3, D3) :-
+get_valid_shelf(180, 5, 6, 5, 6, _).
+get_valid_shelf(180, 5, 10, 5, 10, 'SM_').
+get_valid_shelf(180, _, _, W3, D3, 'SM_') :-
     W3 is 5, 
     D3 is 10.
 
-get_valid_shelf(160, 4, 10, 4, 10).
-get_valid_shelf(160, 5, 10, 5, 10).
-get_valid_shelf(160, 6, 10, 6, 10).
-get_valid_shelf(160, _, _, W3, D3) :-
-    W3 is 6,
-    D3 is 10.
+get_valid_shelf(160, 4, 10, 4, 10,_).
+get_valid_shelf(160, 5, 10, 5, 10, _).
+get_valid_shelf(160, 6, 10, 6, 10, 'SM_').
+get_valid_shelf(160, _, _, 6, 10, 'SM_').
 
-get_valid_shelf(200, 5, 6, 5, 6).
-get_valid_shelf(200, 5, 10, 5, 10).
-get_valid_shelf(200, 5, 12, 5, 12).
-get_valid_shelf(200, 6, 6, 6, 6).
-get_valid_shelf(200, 6, 7, 6, 7).
-get_valid_shelf(200, 6, 10, 6, 10).
-get_valid_shelf(200, 6, 12, 6, 12).
-get_valid_shelf(200, 7, 10, 7, 10).
-get_valid_shelf(200, 7, 12, 7, 12).
-get_valid_shelf(200, _, _, W3, D3) :-
+get_valid_shelf(200, 5, 6, 5, 6, _).
+get_valid_shelf(200, 5, 10, 5, 10, _).
+get_valid_shelf(200, 5, 12, 5, 12, _).
+get_valid_shelf(200, 6, 6, 6, 6, _).
+get_valid_shelf(200, 6, 7, 6, 7, _).
+get_valid_shelf(200, 6, 10, 6, 10, _).
+get_valid_shelf(200, 6, 12, 6, 12, _).
+get_valid_shelf(200, 7, 10, 7, 10, _).
+get_valid_shelf(200, 7, 12, 7, 12, _).
+get_valid_shelf(200, _, _, W3, D3, _) :-
     W3 is 7,
     D3 is 12.
 
 
-get_valid_layer(4, 10, 4, 10).
-get_valid_layer(4, _, 4, 10).
-get_valid_layer(5, 6, 5, 6).
-get_valid_layer(5, 7, 5, 7).
-get_valid_layer(5, 10, 5, 10).
-get_valid_layer(5, 12, 5, 12).
-get_valid_layer(5, _, 5, 12).
-get_valid_layer(6, 10, 6 ,10).
-get_valid_layer(6, 12, 6 ,12).
-get_valid_layer(6, _, 6 ,12).
-get_valid_layer(7, _, 6, 12).
-get_valid_layer(_, _, 6, 12).
+get_valid_layer(4, 10, 4, 10, _).
+get_valid_layer(4, _, 4, 10, _).
+get_valid_layer(5, 6, 5, 6, _).
+get_valid_layer(5, 7, 5, 7, _).
+get_valid_layer(5, 10, 5, 10, _).
+get_valid_layer(5, 12, 5, 12, _).
+get_valid_layer(5, _, 5, 12, _).
+get_valid_layer(6, 10, 6 ,10, _).
+get_valid_layer(6, 12, 6 ,12, TypeName):-
+    TypeName = 'SM_'.
+get_valid_layer(6, _, 6 ,12, 'SM_').
+get_valid_layer(7, _, 6, 12, 'SM_').
+get_valid_layer(_, _, 6, 12, 'SM_').
 
 get_valid_blayer(4, 10, 4, 10).
 get_valid_blayer(4, _, 4, 10).
